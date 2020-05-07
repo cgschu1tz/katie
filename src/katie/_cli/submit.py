381135d9d33eb6b72bc625ae0979ecd1c1bb0f56
cgschu1tz <cgschultz@ksu.edu>
2020-05-07 11:17:45 -0500

    Remove force option and prompt

--- src/katie/_cli/submit.py
+++ src/katie/_cli/submit.py
@@ -22,9 +22,6 @@ def main():
         default=pathlib.Path.home() / pathlib.Path(".kattisrc"),
         help="a file that some some judges (Kattis) require to login (defaults to `~/.kattisrc`)",
     )
-    parser.add_argument(
-        "-f", "--force", action="store_true", help="Don't prompt before submission.",
-    )
     parser.add_argument("-l", "--language")
     parser.add_argument("-m", "--main-class")
     parser.add_argument("url")
@@ -33,9 +30,7 @@ def main():
     args = parser.parse_args()
     kwargs = vars(args)
     utility.init_root_logger(args.logging_level)
-    positive_replies = ["y", "yes"]
     problem = utility.identify_problem(args.url)
-    if args.force or input("Submit? [yN] ").lower() in positive_replies:
-        verdict = problem.submit(**kwargs)
-        print(verdict)
-        webbrowser.open(verdict)
+    verdict = problem.submit(**kwargs)
+    print(verdict)
+    webbrowser.open(verdict)
